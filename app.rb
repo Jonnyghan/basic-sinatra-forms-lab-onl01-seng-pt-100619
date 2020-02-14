cbb89d5324c772644016845ffc97ec8d5eded154
Jonathan Ghandforoush <jonnyghan@gmail.com>
2020-02-14 23:37:06 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,17 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get "/newteam" do
+    erb :newteam
 
+  end
+
+  post "/team" do 
+    @tn=params[:tn].to_s
+    @=params[].to_s   
+    @=params[].to_s
+    @=params[].to_s
+    @=params[].to_s
+    @=params[].to_s
+    @=params[].to_s
 end
